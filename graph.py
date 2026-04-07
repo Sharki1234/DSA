b91a8369156c8f56b34521d389a54ed348372436
Thanishka Lalam Venkata <thanishkavenkata28@gmail.com>
2026-04-07 20:01:28 +0100

    done bfs for graphs

--- graph.py
+++ graph.py
@@ -20,6 +20,8 @@ class Graph:
                     queue.append(num)
                     visited[num] = True
         return result
+    def count_neighbours(self,node):
+        return len(self.adj_list[node-1])
 
 
 g = Graph(6)
@@ -27,4 +29,6 @@ g.add_edge(3,5)
 g.add_edge(4,3)
 g.add_edge(5,6)
 print(g.bfs(3))
+print(g.count_neighbours(3))
+print(g.adj_list)
 
